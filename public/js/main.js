d365e1228c6f32d4c75848e37f7cf8ca496211a2
CoreyOliver <coreyoliver.dev@gmail.com>
2023-03-27 16:05:07 -0400

    form add and clone complete. form button click.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,19 @@
 //in line function call to add line for pallet builder
 function addLineForCarton(e) {
     let clone = e.parentNode.cloneNode(true)
+    clone.innerText.value = ''
     e.parentNode.after(clone)
-    console.log(clone)
+    // console.log(clone)
 }
 
 function clickThatButton(e) {
     if(e.key === 'Enter') {
-        // e.preventDefault()
+        let myButton = e.srcElement.parentNode.children[1]
+        let childPlacement = (myButton.parentNode.parentNode.children.length)
         //on enter select the button to add another line
-        // e.lastChild().click()
-        // console.log(e.parentNode.lastChild)
-
+        myButton.click()
+        
+        myButton.parentNode.parentNode.children[childPlacement].children[0].focus()
     }
 }
 
